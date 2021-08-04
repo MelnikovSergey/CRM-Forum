5a0563c3b1ad8472818be04ad3f816d80c30f653
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2021-08-05 00:10:07 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,14 @@
 <?php
+
 # Конфигурация
-echo 'Конфигурация';
+$servername = 'localhost';
+$username = 'crm_user';
+$password = 'crm_pass';
+$dbname = 'crm_db';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error) {
+	echo 'Соединение не удалось: ' . $conn->connect_error;
+	exit;
+}
