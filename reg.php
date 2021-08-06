dc2b4e3459fb44a386d783ab79eb236067ca86c8
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2021-08-06 23:56:49 +0300

    Update reg.php

--- reg.php
+++ reg.php
@@ -1,3 +1,12 @@
+<h1>Регистрация</h1>
+
+<form action="reg.php" method="POST">
+	<input type="text" name="name" value="" placeholder="Имя">
+	<input type="text" name="email" value="" placeholder="E-mail">
+	<input type="password" name="password" value="" placeholder="Пароль">
+	<input type="submit" value="Отправить">
+</form>
+
 <?php
 # Регистрация
 echo 'Регистрация';
