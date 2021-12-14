47d21e8df05d85ba5958d4c77b24401fb640a1c1
unknown <sergeyandmelnikov@gmail.com>
2021-12-14 17:33:17 +0300

    implemented data storage and loading layer

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,64 @@
 class App {
 
-	run() {
-		console.log('Start application is Up ;)');
+	constructor(data, tableId) {
+		this._store = new Store(data);
+		this._tableId = tableId;
+
+		this._store.subscribe(this._changeData.bind(this));
+		
+		// this._initData(data);
+		this._store.notifyListeners();
 	}
 
+	/*
+	_initData(data) {
+		data.forEach((record) => {
+			this._store.addRecord(record);
+		});
+	}
+	*/
+
+	run() {
+		this._store.addRecord({
+			first: 'Name',
+			last: 'LastName',
+			action: 'Some action'
+		});
+		
+		console.log('Ok');
+	}
+
+	_changeData = () => {
+		const newData = this._store.getRecords();
+		const parentTable = document.getElementById(this._tableId);
+
+		parentTable.innerHTML = '';	
+	
+		newData.forEach(rowData => {
+			const row = this._renderRowData(rowData);
+			parentTable.appendChild(row);
+		});
+
+		// debugger;
+
+	}
+
+	_renderRowData(rowData) {
+		const row = document.createElement('tr');
+		row.appendChild(this._generateTd(rowData.id));
+		row.appendChild(this._generateTd(rowData.first));
+		row.appendChild(this._generateTd(rowData.last));
+		row.appendChild(this._generateTd(rowData.action));
+		
+		return row;
+
+	}
+
+	_generateTd(tdData) {
+		const td = document.createElement('td');
+		td.appendChild(document.createTextNode('tdData'));
+
+		return td;
+
+	}
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,42 @@
 class Store {
-	constructor(initState) {
+	constructor(initState = []) {
 		this._id = 0;
 		this._state = {};
+
+		initState.forEach((data) => {
+			this._addDataRecord(data);
+		});
+
+		this._handlers = [];
+	}
+
+	_addDataRecord(data) {
+		data.id = this._id;
+		this._state[this._generateId()] = data;
 	}
 
 	addRecord(data) {
-		this._state[this._generateId()] = data;
+		this._addDataRecord(data);
+		this.notifyListeners();
+	}
+
+	getRecords() {
+		const fields = Object.keys(this._state);
+
+		return fields.map(fieldId => {
+			return { ...this._state[fieldId] };
+		});
 	}
 
 	_generateId() {
 		return 'my_id_' + this._id++;
+	}
+
+	subscribe(callback) {
+		this._handlers.push(callback);
+	}
+
+	notifyListeners() {
+		this._handlers.forEach((call_Back) => call_Back());
 	}
 }
